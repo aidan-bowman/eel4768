7e8a51cc97e0e9b3c8eca52d02927807eedd21e8
cheesepizza2 <crndog2006@gmail.com>
2026-09-02 15:37:24 -0400

    fixes to mult.s + ,gitignore

--- submissions/phase_1/mult.s
+++ submissions/phase_1/mult.s
@@ -1,37 +1,35 @@
 .data
-a: .word 34
-b: .word 27
+a: .word 22
+b: .word 59
 c: .word 0
 
 .text
 .global _main
 
 main:
-    la   t0, a          # address of a
-    la   t1, b          # address of b
-    la   t2, c          # address of c
+    lui t0, zero, 0x10010   # data segment address
 
-    lw   t3, 0(t0)      # t3 = A (multiplicand, will be shifted left)
-    lw   t4, 0(t1)      # t4 = B (multiplier, will be shifted right)
+    lw   t1, 0(t0)      # t1 = A (multiplicand, will be shifted left)
+    lw   t2, 4(t0)      # t2 = B (multiplier, will be shifted right)
 
-    li   t5, 0           # t5 = result accumulator, C = 0
+    li   t3, 0           # t3 = result accumulator, C = 0
 
 mul_loop:
-    beq  t4, x0, mul_done   # if B == 0, done
+    beq  t2, x0, mul_done   # if B == 0, done
 
-    andi t6, t4, 1           # t6 = B & 1 (check lowest bit)
-    beq  t6, x0, skip_add    # if bit is 0, skip the add
+    andi t4, t2, 1           # t4 = B & 1 (check lowest bit)
+    beq  t4, x0, skip_add    # if bit is 0, skip the add
 
-    add  t5, t5, t3          # C += A
+    add  t3, t3, t1          # C += A
 
 skip_add:
-    slli t3, t3, 1            # A <<= 1
-    srli t4, t4, 1            # B >>= 1 (logical shift, B treated as unsigned)
+    slli t1, t1, 1            # A <<= 1
+    srli t2, t2, 1            # B >>= 1 (logical shift, B treated as unsigned)
 
     j    mul_loop
 
 mul_done:
-    sw   t5, 0(t2)      # store result into c
+    sw   t3, 8(t0)          # store result into c
 
 done:
     # terminate execution (matching example convention)
