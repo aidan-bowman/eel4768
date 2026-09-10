ba5b6f5ede6bbae1acf6e19afda6f32bc73004c5
lordlo2006 <lordlo2006@gmail.com>
2026-09-10 11:11:06 -0400

    Update decoder.v bruh again

--- submissions/phase_2/decoder.v
+++ submissions/phase_2/decoder.v
@@ -140,7 +140,7 @@ module decoder (
     assign o_rd_sel = (is_arr | is_arr_imm | is_auipc) ? 4'b0001 :
                      (is_lui)                         ? 4'b0010 :
                      (is_jal | is_jalr)               ? 4'b0100 :
-                     (is_load)                        ? 4'b1000;
+                     (is_load)                        ? 4'b1000 : 4'b0000;
    
    assign o_pc_sel = is_jalr;
 
